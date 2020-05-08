de34ac1fa40ee5d8d8fd58a88db2b515b7a2dd8f
syuan <syuan.jhong1989@gmail.com>
2020-05-08 17:26:33 +0800

    finish

--- massage.py
+++ massage.py
@@ -6,6 +6,6 @@ from linebot.models import *
 #ImagemapSendMessage(組圖訊息)
 def img_message():
     message = ImagemapSendMessage(
-        base_url="https://github.com/syuan0327/linebot1/blob/master/deer.jpg",
+        base_url="https://github.com/syuan0327/linebot1/blob/master/deer.jpg?raw=true",
     )
     return message
