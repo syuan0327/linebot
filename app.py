f2b01fdca69efc32015a8947d36d1136bb14ea50
syuan <syuan.jhong1989@gmail.com>
2020-05-14 21:34:10 +0800

    finish

--- app.py
+++ app.py
@@ -78,9 +78,15 @@ def handle_sticker_message(event):
     else:
         message = TextSendMessage(text=msg)
         line_bot_api.reply_message(event.reply_token, message)
-        line_bot_api.reply_message(
+
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    pid = event.message.package_id
+    sid = event.message.sticker_id
+    line_bot_api.reply_message(
         event.reply_token, 
         StickerSendMessage(package_id=pid, sticker_id=sid)
+    )
 
     
 
