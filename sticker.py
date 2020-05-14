cc3275435b55525237768c6baedbe0a492cadcf7
syuan <syuan.jhong1989@gmail.com>
2020-05-14 21:13:40 +0800

    finish

--- sticker.py
+++ sticker.py
@@ -2,7 +2,7 @@ from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-def sticker()
+def sticker(event)
     pid = event.message.package_id
     sid = event.message.sticker_id
     line_bot_api.reply_message(
