93a19ed44b7ba86ac70f8b14a2d601502b0f5e50
syuan <syuan.jhong1989@gmail.com>
2020-05-26 13:15:40 +0800

    new

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def handle_sticker_message(event):
 
     line_bot_api.reply_message(
         event.reply_token, 
-        StickerSendMessage(sticker)
+        StickerSendMessage((package_id, sticker_id)=sticker)
     )
 
 
