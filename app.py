cbdaa065aa189654f3297c398e181163e960a6b7
syuan <syuan.jhong1989@gmail.com>
2020-08-21 23:30:04 +0800

    make it better

--- app.py
+++ app.py
@@ -45,14 +45,13 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message=[]
     msg = event.message.text
     if 'yes'or'Yes' in msg:
-        
-        message.append( TextSendMessage('第一題') )
-        message.append( TextSendMessage('收到魔動訊號 北極星光閃耀\n飛向快樂星球\n稍息立正稍息立正站好\n超時空的邂逅 銀河系外接頭\n說出你的暗號 ') )
-        message.append( TextSendMessage('請說出你的暗號：') )
-        line_bot_api.reply_message(event.reply_token, message )
+        message1=[]
+        message1.append( TextSendMessage('第一題') )
+        message1.append( TextSendMessage('收到魔動訊號 北極星光閃耀\n飛向快樂星球\n稍息立正稍息立正站好\n超時空的邂逅 銀河系外接頭\n說出你的暗號 ') )
+        message1.append( TextSendMessage('請說出你的暗號：') )
+        line_bot_api.reply_message(event.reply_token, message1 )
     if '魔動北極星' in msg:
         message.append = TextSendMessage(text='哇~好聰明恭喜你答對了!')
         sticker = StickerSendMessage(package_id=11537, sticker_id=52002734 ) 
