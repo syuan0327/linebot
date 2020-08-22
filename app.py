1a2c392f702e3b439d5bac2c1d358e4c0dbf4731
syuan <syuan.jhong1989@gmail.com>
2020-08-22 20:54:19 +0800

    make it better

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def handle_message(event):
         message3 = TextSendMessage(text='不知道嗎?：')
         line_bot_api.reply_message(event.reply_token, [message1,message2])
         time.sleep(3)
-        line_bot_api.push_message(TextSendMessage('U1efff3aef850fcbcf090fbc62dfe233b',text='Hello World!'))
+        line_bot_api.push_message('U1efff3aef850fcbcf090fbc62dfe233b',  TextSendMessage(text='Hello World!'))
     
 
     else:
