7231e5e795e46acbec65cb843ccbc1a0ffc138cb
syuan <syuan.jhong1989@gmail.com>
2020-08-22 20:58:31 +0800

    make it better

--- app.py
+++ app.py
@@ -73,10 +73,10 @@ def handle_message(event):
     if '3' in msg:
         message1 = TextSendMessage(text='第三題')
         message2 = TextSendMessage(text='請輸入密碼：')
-        message3 = TextSendMessage(text='不知道嗎?：')
         line_bot_api.reply_message(event.reply_token, [message1,message2])
-        time.sleep(3)
-        line_bot_api.push_message('U1efff3aef850fcbcf090fbc62dfe233b',  TextSendMessage(text='Hello World!'))
+        time.sleep(10)
+        line_bot_api.push_message('U1efff3aef850fcbcf090fbc62dfe233b',  TextSendMessage(text='不知道嗎?'))
+        line_bot_api.push_message('U1efff3aef850fcbcf090fbc62dfe233b',  TextSendMessage(text='那不如先聽首歌吧!'))
     
 
     else:
